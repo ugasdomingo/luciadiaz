7a209ff3bdd19f5da48ad8329e758fc7eace2d15
Domingo Ugas <89316506+ugasdomingo@users.noreply.github.com>
2022-07-11 16:53:16 -0400

    Env. Database

--- backend/database.ts
+++ backend/database.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 //Connect to MongoDB 
 export const startConnection = async () => {
   try {
-    const db = await mongoose.connect('mongodb+srv://db_user_silicit:eZsoeZPr5KPd9Q29@cluster0.l0syt.mongodb.net/lucia_db');
+    const db = await mongoose.connect(process.env.DB_CONNECTION);
     console.log ('Connected to ' + db.connections[0].name)
   } catch (error) {
     console.error(error);
