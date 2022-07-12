21e817e57e8528e1c570ec3c73bf6f44dab773c4
Domingo Ugas <89316506+ugasdomingo@users.noreply.github.com>
2022-07-12 10:51:54 -0400

    Update database.ts

--- backend/database.ts
+++ backend/database.ts
@@ -1,11 +1,15 @@
 //Tools Import 
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 
 
 //Connect to MongoDB 
 export const startConnection = async () => {
   try {
+<<<<<<< HEAD
+    const db = await mongoose.connect(process.env.DATABASE as string);
+=======
     const db = await mongoose.connect(process.env.DB_CONNECTION);
+>>>>>>> 7a209ff3bdd19f5da48ad8329e758fc7eace2d15
     console.log ('Connected to ' + db.connections[0].name)
   } catch (error) {
     console.error(error);
