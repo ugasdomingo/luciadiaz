9837e1aefc513948d645b1b065f6b2899f22fa03
unknown <ugasdomingo@gmail.com>
2023-10-18 14:01:43 +0200

    Test img to cloudinary

--- frontend/src/static/testsInfo.ts
+++ frontend/src/static/testsInfo.ts
@@ -6,7 +6,7 @@ export const testsInfo = [
         description:
             'Detrás de toda meta que no logramos alcanzar, aun estando muy cerca de alcanzarla, hay una mente que rechaza y bloquea algo alrededor de esa meta. Este test te permitira en sólo 2 minutos descubrir qué es.',
         link: '/despertando/desbloqueo-de-metas',
-        img: 'src/assets/portada-goals.png',
+        img: 'https://res.cloudinary.com/minteados/image/upload/v1697630433/LuciaWeb/portada-goals_ncstey.png',
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const testsInfo = [
         description:
             'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctort.',
         link: '/despertando/arquetipos',
-        img: 'src/assets/portada-archetype.png',
+        img: 'https://res.cloudinary.com/minteados/image/upload/v1697630433/LuciaWeb/portada-archetype_uql8p8.png',
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const testsInfo = [
         description:
             'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctort.',
         link: '/despertando/temperamentos',
-        img: 'src/assets/portada-temper.png',
+        img: 'https://res.cloudinary.com/minteados/image/upload/v1697630433/LuciaWeb/portada-temper_e3leuf.png',
     },
 ];
 
