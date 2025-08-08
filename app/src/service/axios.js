74fefaaff2be006ca52830976d778a7ffba15ec5
unknown <ugasdomingo@gmail.com>
2025-08-08 13:20:59 +0200

    Firs version for desing //5

--- app/src/service/axios.js
+++ app/src/service/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001/api/v3',
+    baseURL: 'https://luciadiaz-f5cj.vercel.app/api/v3',
     timeout: 10000,
     withCredentials: true,
 })
+
+// Interceptors
+// https://luciadiaz-f5cj.vercel.app/
+// http://localhost:3001/api/v3
