9638e2cd6070ee08d0cf8d75173ded0b4770f5b6
unknown <ugasdomingo@gmail.com>
2025-09-19 19:40:40 +0200

    v3 fix vercel bug no deploy 6

--- app/src/service/axios.js
+++ app/src/service/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: '/api/v3',
+    baseURL: 'https://luciadiaz-api.vercel.app/api/v3',
     timeout: 15000,
     withCredentials: true,
 })
 
 // Interceptors
-// https://luciadiaz-f5cj.vercel.app/api/v3
+// https://luciadiaz-api.vercel.app/api/v3
 // http://localhost:3001/api/v3
