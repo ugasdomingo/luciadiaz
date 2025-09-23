6d65ab9f4cf8b703efc990ac0d5a0ede4c53958b
unknown <ugasdomingo@gmail.com>
2025-09-23 12:59:23 +0200

    v3 static icon url modificated

--- app/src/static/services.js
+++ app/src/static/services.js
@@ -3,24 +3,24 @@ export const services = [
         id: 1,
         title: 'Leer un post',
         description: 'Aprende más sobre psicología y cómo aplicarla en tu día a día.',
-        icon: '/public/icon/icon-post.svg'
+        icon: '/icon/icon-post.svg'
     },
     {
         id: 2,
         title: 'Hacer una formación',
         description: 'Practica y aplica herramientas de psicología a tu vida.',
-        icon: '/public/icon/icon-formacion.svg'
+        icon: '/icon/icon-formacion.svg'
     },
     {
         id: 3,
         title: 'Hacer un test',
         description: 'Conoce en profundidad tu mente y descubre tus fortalezas y debilidades.',
-        icon: '/public/icon/icon-test.svg'
+        icon: '/icon/icon-test.svg'
     },
     {
         id: 4,
         title: 'Terapia',
         description: 'Te acompaño personalmente a mejorar tu calidad de vida.',
-        icon: '/public/icon/icon-terapia.svg'
+        icon: '/icon/icon-terapia.svg'
     },
 ]
